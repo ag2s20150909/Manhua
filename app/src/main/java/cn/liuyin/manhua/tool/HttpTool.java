60859665ad564f3835eff635a9196f3b71715cb8
ag2s20150909 <2075260338@qq.com>
2018-01-08 16:21:19 +0800

    fix some problems

--- app/src/main/java/cn/liuyin/manhua/tool/HttpTool.java
+++ app/src/main/java/cn/liuyin/manhua/tool/HttpTool.java
@@ -1,6 +1,13 @@
 package cn.liuyin.manhua.tool;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.Fragment;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.webkit.WebSettings;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -12,6 +19,8 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.URLEncoder;
 
 
@@ -27,46 +36,65 @@ public class HttpTool {
     String authenticity_token;
     String msg;
     OkHttpClient client;
-    public HttpTool(){
+    SharedPreferences preferences;
+
+    public HttpTool() {
         this(APP.getContext());
     }
+
     public HttpTool(Context context) {
+        preferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+
         this.mContext = context;
         client = new OkHttpClient.Builder()
                 .cookieJar(CookieManager.getInstance(context))
                 .build();
     }
 
-    public static String httpGet(String url){
-        String ip="";
-        int port=0;
+    public static String httpGet(String url) {
+        SharedPreferences preferences = APP.getContext().getSharedPreferences("settings", Context.MODE_PRIVATE);
+        String ip = "";
+        int port = 0;
         try {
-            JSONArray ips=new JSONArray(FileTool.readFile("","ips.json"));
-
-            int s=0;
-            ip=ips.getJSONObject(s).getString("ip");
-            port=Integer.parseInt(ips.getJSONObject(s).getString("port"));
-        }
-        catch (Exception e){
+            JSONArray ips = new JSONArray(FileTool.readFile("", "ips.json"));
+            int s = 0;
+            ip = ips.getJSONObject(s).getString("ip");
+            port = Integer.parseInt(ips.getJSONObject(s).getString("port"));
+        } catch (Exception e) {
             e.printStackTrace();
 
         }
-        System.err.println(ip+":"+port);
-        OkHttpClient client = new OkHttpClient.Builder()
-                .cookieJar(CookieManager.getInstance(APP.getContext()))
-                //.proxy(new Proxy(Proxy.Type.HTTP,new InetSocketAddress(ip,port)))
-                .build();
-        Request request = new Request.Builder().get().url(url)
-                .header("Referer", "m.pufei.net")
-                .header("User-Agent",new UserAgent().getUA())
-                .build();
+
+        OkHttpClient.Builder buider = new OkHttpClient.Builder();
+        if (preferences.getBoolean("cookie", true)) {
+            buider.cookieJar(CookieManager.getInstance(APP.getContext()));
+        }
+        if (preferences.getBoolean("proxy", false)) {
+            if (preferences.getBoolean("handproxy", false)) {
+                ip = preferences.getString("proxy_url", "127.0.0.1");
+                port = preferences.getInt("proxy_port", 0);
+            }
+            buider.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, port)));
+        }
+
+
+        OkHttpClient client = buider.build();
+        Request.Builder requestbuilder = new Request.Builder().get().url(url);
+        requestbuilder.header("Referer", "m.pufei.net");
+        if (preferences.getBoolean("useragent", true)) {
+            requestbuilder.header("User-Agent", new UserAgent().getUA());
+        } else {
+            requestbuilder.header("User-Agent", WebSettings.getDefaultUserAgent(APP.getContext()));
+        }
+
+        Request request = requestbuilder.build();
 
         try {
             Response response = client.newCall(request).execute();
             if (response.isSuccessful()) {
-                return new String(response.body().bytes(),"gb2312");
+                return new String(response.body().bytes(), "gb2312");
             } else {
-                return "error:"+response.message()+" errorcode:"+response.code();
+                return "error:" + response.message() + " errorcode:" + response.code();
             }
         } catch (Exception e) {
             return "error:" + e.getMessage();
@@ -75,47 +103,87 @@ public class HttpTool {
     }
 
 
-    public static void getIPs(){
+    public static void getIPs() {
         try {
-            Document doc=Jsoup.connect("http://www.kuaidaili.com/free/intr/").get();
-            Elements list=doc.select("#list").select("tr");
-            JSONArray ips=new JSONArray();
-            for(int i=0;i<list.size();i++){
-                JSONObject dd=new JSONObject();
-                dd.put("ip",list.get(i).select("td").get(0).text());
-                dd.put("port",Integer.parseInt(list.get(i).select("td").get(1).text()));
+            Document doc = Jsoup.connect("http://www.kuaidaili.com/free/intr/").get();
+            Elements list = doc.select("#list").select("tr");
+            JSONArray ips = new JSONArray();
+            for (int i = 0; i < list.size(); i++) {
+                JSONObject dd = new JSONObject();
+                dd.put("ip", list.get(i).select("td").get(0).text());
+                dd.put("port", Integer.parseInt(list.get(i).select("td").get(1).text()));
                 ips.put(dd);
             }
-            FileTool.writeFile("ips.json",ips.toString(4));
-        }
-        catch (Exception e){
+            FileTool.writeFile("ips.json", ips.toString(4));
+        } catch (Exception e) {
 
         }
     }
 
 
-
-
-
     public static String search(Context context, String kw) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (context.checkSelfPermission(Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+                if (context instanceof Activity) {
+                    Activity activity = (Activity) context;
+                    activity.requestPermissions(new String[]{Manifest.permission.INTERNET}, 0);
+                }
+
+            }
+
+        }
+
         try {
             kw = URLEncoder.encode(kw, "gb2312");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
+        String url = "http://m.pufei.net/e/search/?searchget=1&tbname=mh&show=title,player,playadmin,bieming,pinyin&tempid=4&keyboard=" + kw;
 
-        OkHttpClient client = new OkHttpClient.Builder()
-                .cookieJar(CookieManager.getInstance(context))
-                //.proxy(new Proxy(Proxy.Type.HTTP,new InetSocketAddress(ip,port)))
-                .build();
+        SharedPreferences preferences = APP.getContext().getSharedPreferences("settings", Context.MODE_PRIVATE);
+        String ip = "";
+        int port = 0;
+        try {
+            JSONArray ips = new JSONArray(FileTool.readFile("", "ips.json"));
+            int s = 0;
+            ip = ips.getJSONObject(s).getString("ip");
+            port = Integer.parseInt(ips.getJSONObject(s).getString("port"));
+        } catch (Exception e) {
+            e.printStackTrace();
 
-        Request request = new Request.Builder().get().url("http://m.pufei.net/e/search/?searchget=1&tbname=mh&show=title,player,playadmin,bieming,pinyin&tempid=4&keyboard=" + kw).header("Referer", "http://ww.pufei.net/").build();
+        }
+
+        OkHttpClient.Builder buider = new OkHttpClient.Builder();
+        if (preferences.getBoolean("cookie", true)) {
+            buider.cookieJar(CookieManager.getInstance(APP.getContext()));
+        }
+        if (preferences.getBoolean("proxy", false)) {
+            if (preferences.getBoolean("handproxy", false)) {
+                ip = preferences.getString("proxy_url", "127.0.0.1");
+                port = preferences.getInt("proxy_port", 0);
+            }
+            buider.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, port)));
+        }
+
+
+        OkHttpClient client = buider.build();
+        Request.Builder requestbuilder = new Request.Builder().get().url(url);
+        requestbuilder.header("Referer", "m.pufei.net");
+        if (preferences.getBoolean("useragent", true)) {
+            requestbuilder.header("User-Agent", new UserAgent().getUA());
+        } else {
+            requestbuilder.header("User-Agent", WebSettings.getDefaultUserAgent(APP.getContext()));
+        }
+
+        Request request = requestbuilder.build();
+
         try {
             Response response = client.newCall(request).execute();
             if (response.isSuccessful()) {
                 return response.networkResponse().request().url().toString();
             } else {
-                return "error:"+response.message()+" errorcode:"+response.code();
+                return "error:" + response.message() + " errorcode:" + response.code();
             }
         } catch (IOException e) {
             return "error:" + e.getMessage();
@@ -123,13 +191,6 @@ public class HttpTool {
 
 
     }
-
-
-
-
-
-
-
 
 
 }
